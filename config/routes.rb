1906800b792edc50f2538a8a4cdc21229316aa6d
mihomatsuei <addictly_m@yahoo.co.jp>
2020-09-01 07:52:06 +0000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'posts/edit'
-  get 'posts/show'
-  get 'users/edit'
-  get 'users/show'
   devise_for :users
-  # rootパス
+
   root 'homes#top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/about'
+  get 'user/following/:id' => 'users#following', as: 'following'
+  get 'user/followed/:id' => 'users#followed', as: 'followed'
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+  resources :posts do
+   resource :favorites, only: [:create, :destroy]
+   end
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :post_comments, only: [:create, :destroy]
 end
