459c9fb619f3663f64ef552e62516f41e5fe064f
mihomatsuei <addictly_m@yahoo.co.jp>
2020-09-10 10:17:04 +0000

    Start thanks

--- app/controllers/do_our_bests_controller.rb
+++ app/controllers/do_our_bests_controller.rb
@@ -1,18 +1,9 @@
 class DoOurBestsController < ApplicationController
-	def create
-    if params[:post_id]
-      @post_or_comment = Post.find(params[:post_id])
-      @post_id = params[:post_id]
-      @do_our_best_class = "post_#{@post_or_comment.id}"
-      @do_our_best_path = post_do_our_bests_path(@post_or_comment)
-    else
-      @post_or_comment = PostComment.find(params[:post_comment_id])
-      @post_id = @post_or_comment.post_id
-      @do_our_best_class = "post_comment_#{@post_or_comment.id}"
-      @do_our_best_path = post_comment_do_our_bests_path(@post_or_comment)
-    end
-    @user = @post_or_comment.user
-    @post_comments = PostComment.where(post_id: @post_id)
+	before_action :set_do_our_best
+
+
+
+  def create
     do_our_best = current_user.do_our_bests.new(post_id:@post_id)
     do_our_best.post_comment_id = params[:post_comment_id] if params[:post_comment_id]
     do_our_best.save
@@ -20,7 +11,15 @@ class DoOurBestsController < ApplicationController
    end
 
    def destroy
-    if params[:post_id]
+    do_our_best = current_user.do_our_bests.find_by(post_id: @post_id,post_comment_id:params[:post_comment_id])
+    do_our_best.destroy
+    @do_our_bests_count = DoOurBest.where(post_id:@post_id,post_comment_id:params[:post_comment_id]).count
+   end
+
+private
+
+    def set_do_our_best
+      if params[:post_id]
       @post_or_comment = Post.find(params[:post_id])
       @post_id = params[:post_id]
       @do_our_best_class = "post_#{@post_or_comment.id}"
@@ -33,8 +32,8 @@ class DoOurBestsController < ApplicationController
     end
     @user = @post_or_comment.user
     @post_comments = PostComment.where(post_id: @post_id)
-    do_our_best = current_user.do_our_bests.find_by(post_id: @post_id,post_comment_id:params[:post_comment_id])
-    do_our_best.destroy
-    @do_our_bests_count = DoOurBest.where(post_id:@post_id,post_comment_id:params[:post_comment_id]).count
-   end
+
+    end
+
+
 end
